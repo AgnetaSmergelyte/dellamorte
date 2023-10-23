a883cb56c1d521197fa371d915ddb72632608447
spegisima <agneta.smergelyte@gmail.com>
2023-10-23 14:49:52 +0300

    about pic deleted

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import profilePicture from '../images/20131028_165723.jpg';
 
 const About = () => {
     return (
         <div className="about">
-            <div className="d-flex g-1">
-                {/*<div className="flex-1 d-flex j-center a-start p-1 flex-2-lg">*/}
-                {/*    <div className="oval-img-wrapper">*/}
-                {/*        <img src={profilePicture} alt=""/>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-                <div className="flex-1 flex-3-lg p-1 d-flex f-col a-center text-center g-1">
+                <div className="p-1 d-flex f-col a-center text-center g-1">
                     <h3 className="mb-1">- Apie mane -</h3>
                     <p>Nupiešiau šviesą savo lange, nes nenorėjau matyti užterštos šviesos.</p>
                     <p>Garsiai šaukiau, kad negirdėčiau triukšmo.</p>
@@ -19,7 +12,6 @@ const About = () => {
                     <p>Bėgau, nes norėjau, kad kas nors pagautų.</p>
                     <p>Skridau tam, kad kai nukrisiu, galėčiau vėl pakilti.</p>
                 </div>
-            </div>
         </div>
     );
 };
